e5e051b6c0bc9711a8a8b2bcbefc43903a919ede
mhdsafar <muhammedsafar9567.com>
2025-10-31 10:29:17 +0530

    latest commit

--- src/pages/Moviedetails.jsx
+++ src/pages/Moviedetails.jsx
@@ -56,18 +56,17 @@ const Moviedetails = () => {
               </div>
         </div>
       </div>
-              <p>Your Favourite cast</p>
+              <p className='text-lg font-medium mt-20'>Your Favourite casts</p>
               <div className='overflow-x-auto no-scrollbar mt-8
               pb-4'>
                 <div className='flex items-center gap-4 w-max px-4'>
-              { show.movie.casts.slice(0,12).map(()=>(
+              { show.movie.casts.slice(0,12).map((cast,index)=>(
                 <div key={index} className='flex flex-col items-center text-center'>
-                  <img src={cast.profile_path} alt="" className='
-                   rounded-full h-20 md:h-20 aspect-square object-cover'/>
+                  <img src={cast.profile_path} alt=""
+                   className='rounded-full h-20 md:h-20 aspect-square object-cover'/>
                    <p className='font-medium text-xs mt-3'>{cast.name}</p>
                 </div>
-              )
-            )}
+              ))}
                 </div>
               </div>
 
